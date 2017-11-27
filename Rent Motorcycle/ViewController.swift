4bc55e5e6d9f947a21fdecd88b00ffb3b8a54976
hanifsyaks <32662183+hanifsyaks@users.noreply.github.com>
2017-11-27 14:45:23 +0700

    Initial Commit

---
+++ Rent Motorcycle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rent Motorcycle
+//
+//  Created by muqorrobinaimar on 11/27/17.
+//  Copyright © 2017 muqorrobin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
